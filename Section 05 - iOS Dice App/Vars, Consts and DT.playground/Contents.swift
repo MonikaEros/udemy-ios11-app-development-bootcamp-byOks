8f35d0f183ab3b8ac8d7f72aa51284f5d5b76fe0
allwak <oksamytny@phystech.edu>
2019-09-24 17:45:11 +0300

    Section 5 added

---
+++ Section 05 - iOS Dice App/Vars, Consts and DT.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var myAge : Int = 23
+
+myAge = 24
+
+let myName : String = "Yuriy"
+
+let mySurname : String = "Oksamytny"
+
+let myAgeInTenYears = myAge + 10
+
+let myFullName = "\(myName) \(mySurname)"
+
+
