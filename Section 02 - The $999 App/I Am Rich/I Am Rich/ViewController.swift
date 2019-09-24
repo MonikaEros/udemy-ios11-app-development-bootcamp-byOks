f57fd52ec977adf42fa4b2a9b103dec446551b3f
allwak <oksamytny@phystech.edu>
2019-09-24 17:43:20 +0300

    Section 2 added

---
+++ Section 02 - The $999 App/I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Yuriy Oksamytny on 11/09/2019.
+//  Copyright © 2019 Yuriy Oksamytny. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
